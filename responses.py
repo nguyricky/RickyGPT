84106c98d26b8ce4d6bc0083a1d0f5b88b11d8a1
Ricky Nguyen <nguyricky@gmail.com>
2023-09-27 21:25:27 -0700

    Setup integration with GPT 3.5 LLM

--- responses.py
+++ responses.py
@@ -1,12 +1,28 @@
+from decouple import config
+import openai
+
+openai.api_key = config('OPENAI_TOKEN')
+
 def get_response(message: str) -> str:
     p_message = message.lower()
+    
+    if p_message.startswith('!ask'):
+        response = openai.Completion.create (
+            model = 'gpt-3.5-turbo',
+            prompt = p_message[6:],
+            max_tokens=5000,
+            n=1,
+            stop=None
+        )
+        
+        return response.choices[0].text
     
     if p_message == "!help":
         return (
         "**RickyGPT - Discord Chatbot**\n\n"
         "Hello! I'm RickyGPT, your friendly Discord chatbot powered by OpenAI's GPT-3. I'm here to assist you with various commands and features. Below is a list of available commands and their usage:\n\n"
-        "1. `!chat <your_message>`:\n"
-        "   - Initiate a chat conversation with me. Just type `!chat` followed by your message, and I'll respond accordingly.\n\n"
+        "1. `!ask <your_message>`:\n"
+        "   - Initiate a chat conversation with me. Just type `!ask` followed by your message, and I'll respond accordingly.\n\n"
         "2. `!setprefix <new_prefix>`:\n"
         "   - Change my command prefix to something other than `!`. Replace `<new_prefix>` with your desired prefix to customize how you interact with me.\n\n"
         "3. `!help`:\n"
